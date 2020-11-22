d01d57732541147d798837f7685f77bf5ed2b02f
Andreas Kugel <info@digital-codes.de>
2020-11-22 12:13:50 +0100

    Wit hdate range

--- server/srv.php
+++ server/srv.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 /* fill database paramteres in config.ini */
+/*$cfg = parse_ini_file("../../files/iot/config.ini", false);*/
 $cfg = parse_ini_file("config.ini", false);
 
 /* we can check parameters via a filter like so: */
@@ -16,13 +17,27 @@ values failing the filter condition (e.g. validate int) are FALSE
 
 $parms = array(
 	"last" => FILTER_VALIDATE_INT,
+	"days" => FILTER_VALIDATE_INT,
 	"sens" => FILTER_VALIDATE_INT
 );
 
 $args = filter_input_array(INPUT_GET, $parms,TRUE);
-if ($args["last"])
-	; /* do something, like echo "Last" . PHP_EOL; */
 
+$last = (false | ($args["last"] > 0));
+// default period: 14 
+$per = ($args["days"] ? $args["days"] : 14); /* days */
+
+// date computation
+$fmt = "Y-m-d h:m:s";
+date_default_timezone_set("Europe/Berlin");
+// now
+$d0 = date($fmt);
+//echo "Now: " . $d0 . PHP_EOL;
+// target date
+$d1 = date_create($d0);
+date_sub($d1, date_interval_create_from_date_string($per . " days"));
+$d2 = date_format($d1, $fmt);
+//echo "New: " . $d2 . PHP_EOL;
 
 $pdo = new PDO('mysql:host=' . $cfg["dbserv"] . ';dbname=' . $cfg["dbname"] , $cfg["dbuser"], $cfg["dbpass"]);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
@@ -32,6 +47,11 @@ $query = "SELECT id,req,count,co2,bat,pres,hum,temp,light,rssi,date,pkt,rep from
 /* options to select sensor and to get only the last entry */
 if ($args["sens"]) 
 	$query .= " where id = " . $args["sens"];
+else
+	$query .= " where id >= 0"; // defaults to all
+
+// period
+$query .= " and `date` > \"" . $d2 . "\"";
 
 $query .= " order by `date`";
 
